595c2e6195d29b66b4276fed6ad8c68ede97ef07
lushisang <lushisang@gmail.com>
2024-06-07 17:04:34 +0800

    chore: update to new version VitePress

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run docs:build
+pnpm run docs:build
 
 # navigate into the build output directory
 cd docs/.vitepress/dist
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:lushisang/lushisang.github.io.git master
+git push -f git@github.com:andyluss/andyluss.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
--- docs/.vitepress/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  title: '陆仕桑的个人网站',
-  description: 'Just for fun.'
-}
