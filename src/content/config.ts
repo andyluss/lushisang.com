416b2d98e96d06dd320a72ce7287fb94073978da
lushisang <lushisang@gmail.com>
2024-06-26 12:48:53 +0800

    content: update

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,16 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const diary = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+export const collections = { blog, diary };
