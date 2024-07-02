b2650fd08180aabcc0716b427c0f361a470a982e
lushisang <lushisang@gmail.com>
2024-07-02 13:44:33 +0800

    content: update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "陆仕桑的个人站点";
+export const SITE_TITLE = "陆仕桑";
 export const SITE_DESCRIPTION = "👏欢迎来到我的个人站点！";
