46d658bc6abb98052b21fb9dce0c4a95f7270b66
lushisang <lushisang@gmail.com>
2024-07-04 17:41:18 +0800

    content: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lushisang.com",
-  integrations: [mdx(), sitemap(), vue({ devtools: true })]
+  integrations: [
+    mdx(),
+    sitemap(),
+    vue({ devtools: true }),
+    tailwind(),
+  ]
 });
