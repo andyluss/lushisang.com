c624edc3276d15c2f0764d069102ff86fe2ae260
lushisang <lushisang@gmail.com>
2024-07-05 15:56:53 +0800

    feat: add tag, improve layout

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,41 +1,30 @@
 import { defineCollection, z } from "astro:content";
 
+const commonSchema = z.object({
+  title: z.string(),
+  tags: z.string().array().optional(),
+  description: z.string().optional(),
+  // Transform string to Date object
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  heroImage: z.string().optional(),
+});
+
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: commonSchema,
 });
 
 const xyy = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: commonSchema,
 });
 
 const tech = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: commonSchema,
 });
 export const collections = { blog, xyy, tech };
