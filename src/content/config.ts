cf94db007cbcaff523a96a87c5fce0ad70dab132
lushisang <lushisang@gmail.com>
2024-07-05 12:45:25 +0800

    feat: add tech blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -25,4 +25,17 @@ const xyy = defineCollection({
     heroImage: z.string().optional(),
   }),
 });
-export const collections = { blog, xyy };
+
+const tech = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+export const collections = { blog, xyy, tech };
