512880352231ab8381c728b590c4a5340fb630fc
Andy Lu <lushisang@gmail.com>
2024-08-24 09:00:41 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "🧚逍遥猫🐈";
+export const SITE_TITLE = "🧚清新生活逍遥猫🐈";
 export const SITE_DESCRIPTION = "🏝️实践新自然家园";
