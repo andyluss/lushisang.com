01748604d1673a302c6ec6faf9ddda16e7d1ee0f
lushisang <lushisang@gmail.com>
2024-09-13 20:40:43 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "🐈逍遥猫";
-export const SITE_DESCRIPTION = "🏝️实践新自然家园";
+export const SITE_TITLE = "❄️逍遥猫🐈";
+export const SITE_DESCRIPTION = "🏝️实践新自然家园🏡";
