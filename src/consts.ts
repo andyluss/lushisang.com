98badf2045f6b34e73109e66a22441e805708b10
lushisang <lushisang@gmail.com>
2024-10-06 07:01:10 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "❄️逍遥猫🐈";
-export const SITE_DESCRIPTION = "🏝️🧚实践以人为本的🌳🏡新自然养生园";
+export const SITE_DESCRIPTION = "🏝️🧚实践以人为本🌳🏡新自然养生园";
