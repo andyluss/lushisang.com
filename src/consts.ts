46855555053c1493515dd6039bf3c49fabab4a31
Andy Lu <lushisang@gmail.com>
2024-10-05 16:10:56 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "❄️逍遥猫🐈";
-export const SITE_DESCRIPTION = "🏝️实践以人为本的新自然心灵家园🏡";
+export const SITE_DESCRIPTION = "🏝️🧚实践以人为本的新自然心灵家园🏡";
