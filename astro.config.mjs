d3562ddce7c3f3d2b0df7484d36aeca8ba682ca5
lushisang <lushisang@gmail.com>
2025-02-16 18:29:18 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+import * as compiler from "vue/compiler-sfc";
 import tailwind from "@astrojs/tailwind";
 
 import db from "@astrojs/db";
@@ -9,5 +10,11 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lushisang.com",
-  integrations: [mdx(), sitemap(), vue({ devtools: true }), tailwind(), db()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    vue({ compiler, devtools: true }),
+    tailwind(),
+    db(),
+  ],
 });
