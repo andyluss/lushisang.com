a5c7612594dc3b3429305d373bd7175847bc9159
lushisang <lushisang@gmail.com>
2025-03-27 17:47:00 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,8 @@
 export const SITE_TITLE = "🧚逍遥猫数字花园🌸";
 export const SITE_DESCRIPTION = "🏝️以人为本的生命实践与新自然家园🏡";
 export const COLLECTION_MAP = {
-  xyy: "🧚逍遥游",
-  lab: "🦄猫工社",
-  life: "🐼猫生园",
-  do: "😻干饭猫",
+  xyy: "🧚逍遥游📚",
+  lab: "🦄猫工社💻",
+  life: "🐼猫生园🌸",
+  do: "😻干饭猫🍚",
 };
