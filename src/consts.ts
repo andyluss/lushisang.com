b1a3c806efa5afbe593eb2b94ddaabde48b4c56a
Andy Lu <lushisang@gmail.com>
2025-03-28 07:47:29 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "🧚逍遥猫数字花园🌸";
-export const SITE_DESCRIPTION = "🏝️以人为本的生命实践与新自然家园🏡";
+export const SITE_DESCRIPTION = "🏝️实践以人为本新自然生态家园🏡";
 export const COLLECTION_MAP = {
   xyy: "🧚逍遥游📚",
   lab: "🦄猫工社💻",
