d4cb3c6c0b6e2767ef210b7cd34b1a53c78df85a
Andy Lu <lushisang@gmail.com>
2025-04-04 10:27:03 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "🧚逍遥猫数字港湾🏝️";
-export const SITE_DESCRIPTION = "🐈实践以人为本新自然生态家园🏡";
+export const SITE_TITLE = "🧚逍遥猫数字港湾";
+export const SITE_DESCRIPTION = "实践以人为本新自然家园";
 export const COLLECTION_MAP = {
   xyy: "🧚逍遥游📚",
   lab: "🦄猫工社💻",
